c7ed65f7d185cfb7b0ebe3bb48ba0ff2e1be9e31
Alan Gabriel Dawidowicz <alan.dawidowicz@mediamonks.com>
2017-09-29 10:52:27 -0300

    Add logoFWA and basic got request

--- src/index.js
+++ src/index.js
@@ -1,1 +1,40 @@
-console.log('initial');
+'use strict';
+
+const colors = require('colors');
+const got = require('got');
+
+const handleError = function(error) {
+  console.error(error.response.body);
+};
+
+const getLogoAscii = function() {
+  console.log(
+    colors.yellow.bold(
+      `
+    ______    __  __    ______    ______ _       __    ___ 
+    /_  __/   / / / /   / ____/   / ____/| |     / /   /   |
+     / /     / /_/ /   / __/     / /_    | | /| / /   / /| |
+    / /     / __  /   / /___    / __/    | |/ |/ /   / ___ |
+   /_/     /_/ /_/   /_____/   /_/       |__/|__/   /_/  |_|                                            
+    `
+    )
+  );
+};
+
+got('https://thefwa.com/api/profiles/mediamonks')
+  .then(res => JSON.parse(res.body))
+  .then(data => {
+    if (!data) {
+      console.log(
+        colors.red.bold(
+          'Oops, we have some problems getting the data...try again please.'
+        )
+      );
+      return;
+    }
+
+    getLogoAscii();
+
+    console.log(data.total);
+  })
+  .catch(handleError);
